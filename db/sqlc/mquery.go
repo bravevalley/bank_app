22c72caf993177aa5915f3a508ec296539146e33
Dassyareg <dassyareg@gmail.com>
2023-02-27 11:27:13 +0100

    Created a masterquery

--- db/sqlc/mquery.go
+++ db/sqlc/mquery.go
@@ -49,3 +49,67 @@ func (m *msQ) executeTx(ctx context.Context, fn func(q *Queries) error) error {
 	// Check if there is a commit error
 	return Tx.Commit()
 } 
+
+
+// Transfer process parameters
+type TransferProcessParams struct{
+	Debit  int64 `json:"debit"`
+	Credit int64 `json:"credit"`
+	Amount int64 `json:"amount"`
+}
+
+// Output structs of the database Transaction
+type SuccessfulTransferResult struct{
+	Transfer Transfer `json:"Transfer"`
+	SenderAcc Account `json:"SenderAcc"`
+	ReceiverAcc Account `json:"ReceiverAcc"`
+	SenderTransaction Transaction `json:"SenderTransaction"`
+	ReceiverTransaction Transaction `json:"ReceiverTransaction"`
+}
+
+
+// execTransferTx executes the Transfer transaction, it contains the transfer process prepare for the transfer Tx which includes creating a transfer record, a transaction record for both the sender and receiver and update their acccount ball
+func (m *msQ) execTransferTx(ctx context.Context, arg TransferProcessParams) (SuccessfulTransferResult, error) {
+	var result SuccessfulTransferResult
+	
+	err := m.executeTx(ctx, func(q *Queries) error {
+		var err error
+
+		result.Transfer, err = q.NewTransfer(context.Background(), NewTransferParams{
+			Amount: arg.Amount,
+			Debit: arg.Debit,
+			Credit: arg.Credit,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		result.SenderTransaction, err = q.NewTransaction(ctx, NewTransactionParams{
+			AccNumber: arg.Debit,
+			Amount: -arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+		
+		result.ReceiverTransaction, err = q.NewTransaction(ctx, NewTransactionParams{
+			AccNumber: arg.Credit,
+			Amount: arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		//TODO: Update account balance
+
+
+
+		return nil
+	})
+	
+
+	return result, err
+}
