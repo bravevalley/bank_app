5dc34948f41a5f887dc3dfe148064fc798e36e2c
Dassyareg <dassyareg@gmail.com>
2023-04-29 22:33:13 +0100

    Updated transfer unit test

--- api/transfer_test.go
+++ api/transfer_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -57,6 +58,250 @@ func TestTransferTranx(t *testing.T) {
 			},
 			Expect: func(t *testing.T, res *httptest.ResponseRecorder) {
 				require.Equal(t, http.StatusCreated, res.Code)
+			},
+			Token: func(t *testing.T, req *http.Request, tokenMaker token.TokenMaker) {
+				AddAuthHeader(t, userA.Name, time.Minute, tokenMaker, req, AuthType)
+			},
+		},
+		{
+			Name: "Sender unauthorized",
+			Arg: TransferTranxParams{
+				Sender: userA.AccNumber,
+				Receiver: userB.AccNumber,
+				Amount: amount,
+				Currency: currency,
+			},
+			Stubs: func(m *mockdb.MockMsQ) {
+				m.EXPECT().GetAccount(gomock.Any(), mockdb.AccMatcher(userB.AccNumber, userA.AccNumber)).Times(1).Return(userB, nil)
+	
+				m.EXPECT().ExecTransferTx(gomock.Any(), gomock.Eq(db.TransferProcessParams{
+					Debit: userA.AccNumber,
+					Credit: userB.AccNumber,
+					Amount: int64(amount),
+				})).Times(0).Return(transferReturn(userA, userB, amount), nil)
+
+			},
+			Expect: func(t *testing.T, res *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusUnauthorized, res.Code)
+			},
+			Token: func(t *testing.T, req *http.Request, tokenMaker token.TokenMaker) {
+				AddAuthHeader(t, userA.Name, time.Minute, tokenMaker, req, AuthType)
+			},
+		},
+		{
+			Name: "Sender does not exist",
+			Arg: TransferTranxParams{
+				Sender: userA.AccNumber,
+				Receiver: userB.AccNumber,
+				Amount: amount,
+				Currency: currency,
+			},
+			Stubs: func(m *mockdb.MockMsQ) {
+				m.EXPECT().GetAccount(gomock.Any(), mockdb.AccMatcher(userB.AccNumber, userA.AccNumber)).Times(1).Return(db.Account{}, sql.ErrNoRows)
+	
+				m.EXPECT().ExecTransferTx(gomock.Any(), gomock.Eq(db.TransferProcessParams{
+					Debit: userA.AccNumber,
+					Credit: userB.AccNumber,
+					Amount: int64(amount),
+				})).Times(0).Return(transferReturn(userA, userB, amount), nil)
+
+			},
+			Expect: func(t *testing.T, res *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, res.Code)
+			},
+			Token: func(t *testing.T, req *http.Request, tokenMaker token.TokenMaker) {
+				AddAuthHeader(t, userA.Name, time.Minute, tokenMaker, req, AuthType)
+			},
+		},
+		{
+			Name: "Receiver does not exist",
+			Arg: TransferTranxParams{
+				Sender: userA.AccNumber,
+				Receiver: userB.AccNumber,
+				Amount: amount,
+				Currency: currency,
+			},
+			Stubs: func(m *mockdb.MockMsQ) {
+				m.EXPECT().GetAccount(gomock.Any(), mockdb.AccMatcher(userB.AccNumber, userA.AccNumber)).Times(1).Return(userA, nil)
+
+				m.EXPECT().GetAccount(gomock.Any(), mockdb.AccMatcher(userB.AccNumber, userA.AccNumber)).Times(1).Return(db.Account{}, sql.ErrNoRows)
+	
+				m.EXPECT().ExecTransferTx(gomock.Any(), gomock.Eq(db.TransferProcessParams{
+					Debit: userA.AccNumber,
+					Credit: userB.AccNumber,
+					Amount: int64(amount),
+				})).Times(0).Return(transferReturn(userA, userB, amount), nil)
+
+			},
+			Expect: func(t *testing.T, res *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, res.Code)
+			},
+			Token: func(t *testing.T, req *http.Request, tokenMaker token.TokenMaker) {
+				AddAuthHeader(t, userA.Name, time.Minute, tokenMaker, req, AuthType)
+			},
+		},
+		{
+			Name: "SQL server downtime",
+			Arg: TransferTranxParams{
+				Sender: userA.AccNumber,
+				Receiver: userB.AccNumber,
+				Amount: amount,
+				Currency: currency,
+			},
+			Stubs: func(m *mockdb.MockMsQ) {
+				m.EXPECT().GetAccount(gomock.Any(), mockdb.AccMatcher(userB.AccNumber, userA.AccNumber)).Times(1).Return(db.Account{}, sql.ErrConnDone)
+	
+				m.EXPECT().ExecTransferTx(gomock.Any(), gomock.Eq(db.TransferProcessParams{
+					Debit: userA.AccNumber,
+					Credit: userB.AccNumber,
+					Amount: int64(amount),
+				})).Times(0).Return(transferReturn(userA, userB, amount), nil)
+
+			},
+			Expect: func(t *testing.T, res *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, res.Code)
+			},
+			Token: func(t *testing.T, req *http.Request, tokenMaker token.TokenMaker) {
+				AddAuthHeader(t, userA.Name, time.Minute, tokenMaker, req, AuthType)
+			},
+		},
+		{
+			Name: "SQL server downtime transfer",
+			Arg: TransferTranxParams{
+				Sender: userA.AccNumber,
+				Receiver: userB.AccNumber,
+				Amount: amount,
+				Currency: currency,
+			},
+			Stubs: func(m *mockdb.MockMsQ) {
+				m.EXPECT().GetAccount(gomock.Any(), mockdb.AccMatcher(userB.AccNumber, userA.AccNumber)).Times(1).Return(userA, nil)
+
+				m.EXPECT().GetAccount(gomock.Any(), mockdb.AccMatcher(userB.AccNumber, userA.AccNumber)).Times(1).Return(userB, nil)
+
+				m.EXPECT().ExecTransferTx(gomock.Any(), gomock.Eq(db.TransferProcessParams{
+					Debit: userA.AccNumber,
+					Credit: userB.AccNumber,
+					Amount: int64(amount),
+				})).Times(1).Return(db.SuccessfulTransferResult{}, sql.ErrConnDone)
+
+			},
+			Expect: func(t *testing.T, res *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, res.Code)
+			},
+			Token: func(t *testing.T, req *http.Request, tokenMaker token.TokenMaker) {
+				AddAuthHeader(t, userA.Name, time.Minute, tokenMaker, req, AuthType)
+			},
+		},
+		{
+			Name: "unsupported currency",
+			Arg: TransferTranxParams{
+				Sender: userA.AccNumber,
+				Receiver: userB.AccNumber,
+				Amount: amount,
+				Currency: "ONE",
+			},
+			Stubs: func(m *mockdb.MockMsQ) {
+				m.EXPECT().GetAccount(gomock.Any(), gomock.Eq(userA.AccNumber) ).Times(0).Return(userA, nil)
+
+				m.EXPECT().GetAccount(gomock.Any(), gomock.Eq(userB.AccNumber)).Times(0).Return(userB, nil)
+	
+				m.EXPECT().ExecTransferTx(gomock.Any(), gomock.Eq(db.TransferProcessParams{
+					Debit: userA.AccNumber,
+					Credit: userB.AccNumber,
+					Amount: int64(amount),
+				})).Times(0)
+
+			},
+			Expect: func(t *testing.T, res *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, res.Code)
+			},
+			Token: func(t *testing.T, req *http.Request, tokenMaker token.TokenMaker) {
+				AddAuthHeader(t, userA.Name, time.Minute, tokenMaker, req, AuthType)
+			},
+		},
+		{
+			Name: "Invalid currency",
+			Arg: TransferTranxParams{
+				Sender: userA.AccNumber,
+				Receiver: userB.AccNumber,
+				Amount: amount,
+				Currency: currency,
+			},
+			Stubs: func(m *mockdb.MockMsQ) {
+				m.EXPECT().GetAccount(gomock.Any(), gomock.Eq(userA.AccNumber) ).Times(1).Return(db.Account{
+					AccNumber: userA.AccNumber,
+					Name: userA.Name,
+					Balance: utils.RandomAmount(),
+					Currency: "ONE",
+				}, nil)
+
+				m.EXPECT().GetAccount(gomock.Any(), gomock.Eq(userB.AccNumber)).Times(1).Return(userB, nil)
+	
+				m.EXPECT().ExecTransferTx(gomock.Any(), gomock.Eq(db.TransferProcessParams{
+					Debit: userA.AccNumber,
+					Credit: userB.AccNumber,
+					Amount: int64(amount),
+				})).Times(0)
+
+			},
+			Expect: func(t *testing.T, res *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, res.Code)
+			},
+			Token: func(t *testing.T, req *http.Request, tokenMaker token.TokenMaker) {
+				AddAuthHeader(t, userA.Name, time.Minute, tokenMaker, req, AuthType)
+			},
+		},
+		{
+			Name: "Invalid currency",
+			Arg: TransferTranxParams{
+				Sender: userA.AccNumber,
+				Receiver: userB.AccNumber,
+				Amount: amount,
+				Currency: currency,
+			},
+			Stubs: func(m *mockdb.MockMsQ) {
+				m.EXPECT().GetAccount(gomock.Any(), gomock.Eq(userA.AccNumber) ).Times(1).Return(userA, nil)
+
+				m.EXPECT().GetAccount(gomock.Any(), gomock.Eq(userB.AccNumber)).Times(1).Return(db.Account{
+					AccNumber: userB.AccNumber,
+					Name: userB.Name,
+					Balance: utils.RandomAmount(),
+					Currency: "ONE",
+				}, nil)
+	
+				m.EXPECT().ExecTransferTx(gomock.Any(), gomock.Eq(db.TransferProcessParams{
+					Debit: userA.AccNumber,
+					Credit: userB.AccNumber,
+					Amount: int64(amount),
+				})).Times(0)
+
+			},
+			Expect: func(t *testing.T, res *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, res.Code)
+			},
+			Token: func(t *testing.T, req *http.Request, tokenMaker token.TokenMaker) {
+				AddAuthHeader(t, userA.Name, time.Minute, tokenMaker, req, AuthType)
+			},
+		},
+		{
+			Name: "Invalid input",
+			Arg: TransferTranxParams{
+				Sender: userA.AccNumber,
+				Receiver: userB.AccNumber,
+				Amount: -amount,
+				Currency: currency,
+			},
+			Stubs: func(m *mockdb.MockMsQ) {
+				m.EXPECT().GetAccount(gomock.Any(), mockdb.AccMatcher(userB.AccNumber, userA.AccNumber)).Times(0)
+				m.EXPECT().ExecTransferTx(gomock.Any(), gomock.Eq(db.TransferProcessParams{
+					Debit: userA.AccNumber,
+					Credit: userB.AccNumber,
+					Amount: int64(amount),
+				})).Times(0)
+
+			},
+			Expect: func(t *testing.T, res *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, res.Code)
 			},
 			Token: func(t *testing.T, req *http.Request, tokenMaker token.TokenMaker) {
 				AddAuthHeader(t, userA.Name, time.Minute, tokenMaker, req, AuthType)
